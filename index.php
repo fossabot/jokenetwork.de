5e123499ad976d74ad31841237943279706c42c5
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-23 04:05:50 +0200

    Now: `v2.0.0`

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JokeNetwork - Open Web</title>
+    <meta charset="UTF-8">
+
+    <meta name="description" content="We're a young team of webdesigners and web-developers and we're offering (ads-)free tools and apps as well as open source projects.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+    <link rel="mask-icon" href="/assets/img/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/assets/img/favicon.ico">
+
+    <link href="assets/css/style.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <div class="logo">
+          <a href="//jokenetwork.de"><img src="assets/img/plain.svg" alt="Logo"></a>
+        </div>
+        <nav>
+          <a href="#projects">Projects</a>
+          <a href="//github.com/jokenetwork">GitHub</a>
+        </nav>
+      </div>
+    </header>
+
+    <div class="content">
+     <section class="first" name="intro">
+       <h1>Web-development just reached a new level.</h1>
+       <h2>Everything we do is Open-Source.</h2>
+       <p>We're a young team of webdesigners and web-developers and we're offering free tools and apps!</p>
+       <a class="button" href="#projects">See our current work</a>
+    </section> 
+     <section class="projects" name="projects">
+       <div class="flexbox">
+         <h2>Current Projects</h2>
+         <h3>JokeNetwork Docs</h3>
+         <p>JokeNetworks FAQ included questions that weren't asked frequently.</p>
+         <p>Recently we brought FAQ back to live as JokeNetwork Docs - While the original idea of our faq page was the documentation of our own services, it is now mainly used as an overview and documentation page for MetaExtensions listed in the WHATWG HTML Standard.</p>
+         <a href="//faq.jokenetwork.de">See it in action</a> / <a href="//github.com/jokenetwork/faq">Contribute on GitHub</a> 
+       </div>
+       <div class="flexbox">
+         <div class="presenter"><img src="assets/img/docs.png" alt="JokeNetwork Docs"></div>
+       </div>
+      </section> 
+
+       <section class="contact" name="contact">
+       <h1>Wanna hang out?</h1>
+       <h3>Contribute to one of our projects on <a href="//github.com/jokenetwork">GitHub</a> or drop us an <a href="mailto:info@jokenetwork.de">e-mail</a>.</h3>
+       <h3>Everyone is welcome 💖</h3>
+      </section> 
+
+    </div>
+
+
+   <div class="container">
+    <footer>
+      <p>&copy; 20<?php echo date('y'); ?> JokeNetwork</p>
+      <ul>
+        <li><a href="//github.com/jokenetwork">GitHub</a></li>
+        <li><a href="impressum">Imprint</a></li>
+        <li><a href="privacy-policy">Privacy Policy</a></li>
+      </ul>
+    </footer>
+   </div>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/scroll.js"></script>
+
+    <?php 
+        header('Access-Control-Allow-Origin: https://analytics.jokenetwork.de'); 
+        if (isset($_COOKIE['log']) && $_COOKIE['log'] == "Yes"){echo '<script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="5c67c30a-b240-419a-867d-410abf7eb1db" data-ackee-opts=\'{ "detailed": true }\'></script>';}  
+        elseif (isset($_COOKIE['log']) && $_COOKIE['log'] == "No"){echo '<script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="5c67c30a-b240-419a-867d-410abf7eb1db"></script>';}  
+        else{echo '<script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="5c67c30a-b240-419a-867d-410abf7eb1db" data-ackee-opts=\'{ "detailed": true }\'></script>';} 
+      ?>
+  </body>
+</html>
